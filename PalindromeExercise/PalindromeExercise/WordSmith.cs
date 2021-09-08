b4587449d3d4ecf715319af2adc6de41b14a8a86
Cedric <mrlee.truecoder@gmail.com>
2021-09-08 14:58:32 -0500

    Completed Palindrome Exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome (string word)
+        {
+            char[] charArray = word.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            string revWord= new string(charArray);
+
+            if ( revWord== word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
 
-namespace PalindromeExerciseTests
+namespace PalindromeExercise.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData ("racecar",true)]
+        [InlineData("palindrome", false)]
+        [InlineData("civic", true)]
+        [InlineData("duty", false)]
+        [InlineData("redivider", true)]
 
+        public void IsAPalindrome(string word, bool expected)
+        {
+            //Arrange
+            var tester = new WordSmith();
+
+            //Act
+            var actual = tester.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
